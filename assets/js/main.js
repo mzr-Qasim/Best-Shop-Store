d8d10f9b7d1f6c958d888f766c86f794a4882e07
Muhammad Qasim <malikqasim20051@gmail.com>
2024-11-29 18:58:19 +0500

    Product Category Carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
     loop: true,
+    // autoplay: {
+    //   delay: 4000,
+    //   disableOnInteraction: false,
+    // },
     pagination: {
         el: ".swiper-pagination",
       },
@@ -7,4 +11,34 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+});
+
+
+var swiper = new Swiper(".mySwiper-2", {
+  loop: false,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    567: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+
 });
